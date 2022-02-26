c0df4764cf787296d141c93dfc78c8bbbbc9ee6a
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-02-26 21:26:49 +0900

    Aruments new

--- sniffer/src/main2.rs
+++ sniffer/src/main2.rs
@@ -33,12 +33,28 @@ impl Arguments{
         } else {
             let flag= args[1].clone();
             if flag.contain(-h) || flag.contain("-help" && args.len() == 2{
-                
+                println!("Usage: -j to selete how many threads you want\r\n
+                         -h or -help to show this help maeesage");
+                return Err("help"); 
+            } else if flag.contain("-h" || flag.contains("-help"){
+                return Err("too many arguments");
+            }else if flag.contains("-j"){
+                let ipaddr= match IpAddr::from_str(&args[3]){
+                    Ok(s) => s,
+                    Err(_) => return Err("not a valid IPADDR; must be IPv4 or IPv5")      
+                };
+            
+                let threads= match args[2].parse::<u16>(){
+                    Ok(s) => s,
+                    Err(_) =? return Err("failed to parse thread number")
+                };
+
+                return Ok(Arguments { threads, flag, ipaddr });
+            } else {
+                return Err("invalide syntax");
             }
-        }
-    }
 
 
 fn main() {
     println!("Hello, world!");
-}
+------------}
